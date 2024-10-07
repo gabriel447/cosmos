43be58218daaeee8de4862d156ceba02a2813ae1
Gabriel447 <gabriel_447@live.com>
2024-10-07 20:32:18 -0300

    em massa

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,8 +9,12 @@ class WelcomeController extends Controller
     public function index()
     {
         $user = User::find(1);
+        
+        //setando especificamente cada uma das propriedades..
         $user->email_verified_at = now();
         $user->save();
+        
+        //atualizando em massa
         $user->update(['email_verified_at' => now()]);
         dd($user->email_verified_at);
 
